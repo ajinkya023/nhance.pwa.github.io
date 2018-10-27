265b3d716bfffd270c1fa2eb08c4611fc6c417c2
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-10-27 18:59:15 +0300

    fix

--- src/js/import/swiper.js
+++ src/js/import/swiper.js
@@ -11,3 +11,28 @@ var swiper = new Swiper('.swiper-digital-kit', {
   },
 });
 $('.swiper-pagination-fraction').append('<span class="text">of</span>')
+// swiper.on('touchMove', function(event){
+//   if(!$('.slide__product__function').click()){
+//     console.log('wff')
+//   } else{
+//     event.return
+//     console.log('234')
+//   }
+// })
+$('.slide__product__function').click(function(){
+  swiper.off()
+  console.log('124567')
+})
+
+
+var swiper2 = new Swiper('.swiper-scroll', {
+  direction: 'horizontal',
+  slidesPerView: 'auto',
+  freeMode: true,
+  slidesPerView: 3,
+  spaceBetween: 0,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  mousewheel: true,
+});
