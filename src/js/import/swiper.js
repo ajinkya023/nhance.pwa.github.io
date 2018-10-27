eb14c3577ec67695f8e360130d02f7f0ed398364
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-10-27 19:03:39 +0300

    fix

--- src/js/import/swiper.js
+++ src/js/import/swiper.js
@@ -30,7 +30,7 @@ var swiper2 = new Swiper('.swiper-scroll', {
   slidesPerView: 'auto',
   freeMode: true,
   slidesPerView: 3,
-  spaceBetween: 0,
+  spaceBetween: 30,
   scrollbar: {
     el: '.swiper-scrollbar',
   },
